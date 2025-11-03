2c85d444cd80b376015b44706406973d0766c22f
drewski <drewskiwooskie@gmail.com>
2025-11-03 11:27:37 -0500

    add docker

--- build_files/build.sh
+++ build_files/build.sh
@@ -63,8 +63,20 @@ FEDORA_PACKAGES=(
 # Install all Fedora packages (bulk - safe from COPR injection)
 echo "Installing ${#FEDORA_PACKAGES[@]} packages from Fedora repos..."
 dnf -y install "${FEDORA_PACKAGES[@]}"
+
+echo "Installing Docker from official repo..."
+dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/docker-ce.repo
+dnf -y install --enablerepo=docker-ce-stable \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-ce \
+    docker-ce-cli \
+    docker-compose-plugin \
+    docker-model-plugin
+
 #
 #
-#systemctl enable podman.socket
+systemctl enable podman.socket
 #
 
